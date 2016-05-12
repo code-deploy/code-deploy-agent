473dcc4e316ffd2dcc313be7e30014e7cb8e0875
Hysios Hu <hysios@gmail.com>
2016-05-12 20:16:52 +0800

    upgrade 1.0.17

--- src/runner/suspawn.js
+++ src/runner/suspawn.js
@@ -3,5 +3,5 @@ import log from '../logger';
 
 export function suspawn(command, user, options = {}) {
   log.info(`run command ${command} in ${options['cwd']}`);
-  return spawn('sudo', ['-u', user, `${options['cwd']}/${command}`], {...options});
+  return spawn('sudo', ['-Hiu', user, `${options['cwd']}/${command}`], {...options});
 }
